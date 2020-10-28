2347938231dd92e52c573f063f9ec37bee1fc861
Andrey Shklyaruk <13mailg@gmail.com>
2020-10-28 22:36:25 +0200

    store and axios init

---
+++ src/store/modules/auth.js
@@ -0,0 +1,21 @@
+
+const state = {
+    user: null,
+    posts: null,
+};
+const getters = {
+
+};
+const actions = {
+
+};
+const mutations = {
+
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
