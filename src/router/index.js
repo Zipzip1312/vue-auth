92a4dbc93a8b0f940f828def96491d74b9598df1
Andrey Shklyaruk <13mailg@gmail.com>
2020-10-29 18:12:50 +0200

    add routing guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '../store';
 
 Vue.use(VueRouter)
 
@@ -12,22 +12,25 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: Home
+        component: loadView('Home')
     },
     {
         path: '/register',
         name: 'Register',
-        component: loadView('Register')
+        component: loadView('Register'),
+        meta: { guest: true },
     },
     {
         path: '/login',
         name: 'Login',
-        component: loadView('Login')
+        component: loadView('Login'),
+        meta: { guest: true },
     },
     {
         path: '/posts',
         name: 'Posts',
-        component: loadView('Posts')
+        component: loadView('Posts'),
+        meta: { requiresAuth: true },
     },
 ]
 
@@ -35,6 +38,30 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters.isAuthenticated) {
+            next();
+            return;
+        }
+        next('/login');
+    } else {
+        next();
+    }
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some((record) => record.meta.guest)) {
+        if (store.getters.isAuthenticated) {
+            next("/posts");
+            return;
+        }
+        next();
+    } else {
+        next();
+    }
 })
 
 export default router
