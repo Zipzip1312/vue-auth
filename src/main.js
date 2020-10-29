7aff5b0fea4c44eab8cf5670010d555b6bb7adb5
Andrey Shklyaruk <13mailg@gmail.com>
2020-10-29 15:32:09 +0200

    setup auth module

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router';
 import store from './store';
 import axios from 'axios';
 
+// send all requests with authorization headers, TLS client certificates, cookies (as in our case)
 axios.defaults.withCredentials = true
+// testing backend api
 axios.defaults.baseURL = 'https://gabbyblog.herokuapp.com/';
 
 Vue.config.productionTip = false
