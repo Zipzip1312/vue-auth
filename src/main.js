8cd15bf40ab16207320353bc4498223b3fcd58ca
Andrey Shklyaruk <13mailg@gmail.com>
2020-10-30 12:58:29 +0200

    create axios interceptors to catch expired token

--- src/main.js
+++ src/main.js
@@ -4,10 +4,30 @@ import router from './router';
 import store from './store';
 import axios from 'axios';
 
-// send all requests with authorization headers, TLS client certificates, cookies (as in our case)
-axios.defaults.withCredentials = true
-// fake backend api
+/**
+ * Fake backend API
+ */
 axios.defaults.baseURL = 'http://localhost:3004/';
+/**
+ * Send all requests with authorization headers, TLS client certificates, cookies
+ * required only if we have backend session auth
+ */
+axios.defaults.withCredentials = true
+/**
+ * Handling expired token (Forbidden Requests)
+ * required only if we have backend session auth
+ */
+axios.interceptors.response.use(undefined, function (error) {
+    if (error) {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+
+            originalRequest._retry = true;
+            store.dispatch('LogOut')
+            return router.push('/login')
+        }
+    }
+})
 
 Vue.config.productionTip = false
 new Vue({
